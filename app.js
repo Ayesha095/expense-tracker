88ccca1f5ffe7d84e7371c78ef444d528b938123
Ayesha095 <mshalik03@gmail.com>
2022-06-26 21:44:03 +0500

    files

--- app.js
+++ app.js
@@ -6,12 +6,19 @@ const form=document.getElementById("form");
 const text=document.getElementById("text");
 const amount=document.getElementById("amount");
 
-const dummyTransactions=[
-    {id: 1, text: "Flower",amount:-20},
-    {id: 2, text: "Salary",amount:300},
-    {id: 3, text: "Book",amount:-10},
-    {id: 4, text: "Camera",amount:150},
-];
+// const dummyTransactions=[
+//     {id: 1, text: "Flower",amount:-20},
+//     {id: 2, text: "Salary",amount:300},
+//     {id: 3, text: "Book",amount:-10},
+//     {id: 4, text: "Camera",amount:150},
+// ];
+
+
+const localStorageTransactions =JSON.parse(localStorage.getItem('transactions'));
+let transactions =localStorage.getItem('transactions') !==null ? localStorageTransactions : [];
+
+
+
 
 function addTransactionDOM(transaction){
     const sign = transaction.amount>0 ? "+" : "-";
@@ -20,7 +27,74 @@ function addTransactionDOM(transaction){
 
     const item= document.createElement("li");
 
-    item.classList.add(transaction > 0 ? "plus" : "minus");
+    item.classList.add(transaction.amount > 0 ? "plus" : "minus");
 
-    item.innerHTML= 
+    item.innerHTML= `<h4>${transaction.text}</h4> <span>${sign}$${Math.abs(transaction.amount)} 
+    <i class="fa-solid fa-caret-${icon}"></i><i class="fas fa-trash-alt delete " onclick="removeItem(${transaction.id})></i></span>`;
+
+    list.appendChild(item);
 }
+
+function updateValue(params){
+    const amount = transactions.map(item=> item.amount);
+
+    const total = amount.reduce((a,b)=>(a+=b),0).toFixed(2);
+    const income = amount.filter((item)=>item > 0).reduce((a,b)=>(a+=b), 0).toFixed(2);
+
+    const expense = amount.filter(item=>item > 0).reduce((a,b)=>(a+=b), 0).toFixed(2);
+
+   balance.innerHTML=`$${total}`;
+
+   moneyPlus.innerHTML=`$${income}<i class="fa-solid fa-caret-up"></i>`;
+
+   moneyMinus.innerHTML=`$${Math.abs(expense).toFixed(2)}<i class="fa-solid fa-caret-down"></i>`;
+
+
+}
+
+function randomId(){
+    return Math.floor((Math.random()*1000));
+}
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault()
+
+    if (text.value.trim()==="" || amount.value.trim()=== "") {
+        alert('Please fill your data first');
+        
+    } else {
+        const transaction= {id:randomId(),text:text.value,amount:parseInt(amount.value),};
+        transactions.push(transaction);
+
+        addTransactionDOM(transaction);
+
+        updateValue();
+        updateLocalStorage();
+
+        text.value="";
+
+        amount.value="";
+
+      
+    }
+
+});
+
+function removeItem(id){
+    transactions=transactions.filter(transaction=>transaction.id !== id);
+     updateLocalStorage();
+    init();
+}
+
+function updateLocalStorage(){
+   localStorage.setItem('transactions',JSON.stringify(transactions));
+}
+
+function init(){
+    list.innerHTML="";
+ transactions.forEach(addTransactionDOM);
+
+    updateValue();
+}
+
+init();
